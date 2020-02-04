b2d718d5293f7f7af81acd720eb18e55bf3526e6
dongyajie <dongyajie@ke.com>
2020-02-04 12:43:18 +0800

    remove demo

--- _Web/js/debug/GenerateTestUserSig.js
+++ _Web/js/debug/GenerateTestUserSig.js
@@ -23,7 +23,7 @@ function genTestUserSig(userID) {
    * 进入腾讯云实时音视频[控制台](https://console.cloud.tencent.com/rav ) 创建应用，即可看到 SDKAppId，
    * 它是腾讯云用于区分客户的唯一标识。
    */
-  const SDKAPPID = 1400309161;
+  const SDKAPPID = 0;
 
   /**
    * 签名过期时间，建议不要设置的过短
@@ -43,7 +43,7 @@ function genTestUserSig(userID) {
    * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
    * 文档：https://cloud.tencent.com/document/product/647/17275#Server
    */
-  const SECRETKEY = '97d2f6181b42170d9f1a0e5ad863d6240b799560c049c8b02c0e36565f2034bb';
+  const SECRETKEY = '';
 
   // a soft reminder to guide developer to configure sdkAppId/secretKey
   if (SDKAPPID === '' || SECRETKEY === '') {
